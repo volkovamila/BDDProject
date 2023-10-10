6615cb7ec5eaed38eaf02857bb5c4a559cbc0729
volkovamila <lyudmilka.volkova.70@gmail.com>
2023-10-10 21:18:21 +0800

    corrections

--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -26,7 +26,7 @@ class MoneyTransferTest {
     }
 
     @Test
-    void ShouldTransferFromFirstToSecond() {
+    void shouldTransferFromFirstToSecond() {
         var firstCardInfo = getFirstCardInfo();
         var secondCardInfo = getSecondCardInfo();
         var firsCardBalance = dashboardPage.getCardBalance(firstCardInfo);
@@ -43,7 +43,7 @@ class MoneyTransferTest {
     }
 
     @Test
-    void ShouldGetErrorMessageIfAmountMoreBalance() {
+    void shouldGetErrorMessageIfAmountMoreBalance() {
         var firstCardInfo = getFirstCardInfo();
         var secondCardInfo = getSecondCardInfo();
         var firsCardBalance = dashboardPage.getCardBalance(firstCardInfo);
